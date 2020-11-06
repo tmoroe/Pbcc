62e05d5dcafd7e6c65dea4367b9620f54793682f
Thabang <tmoroe@student.wethinkcode.co.za>
2020-11-06 11:02:36 +0200

    Added task 1-4

---
+++ task3.py
@@ -0,0 +1,5 @@
+def sixtyfive(num1, num2):
+    if (num1 == 65 or num2 == 65 or num1 + num2 ==65):
+        return True
+    else:
+        return False
---
+++ task4.py
@@ -0,0 +1,5 @@
+def three(num1, num2):
+    if ((num1 == 3 or num2 == 3) and "3" in str(num1 + num2)):
+        return True
+    else:
+        return False
